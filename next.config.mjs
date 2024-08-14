fd962d3c1f4625e7b05fdce9ae28f21a3161ef78
chrisweb <chris963@gmail.com>
2024-08-14 20:20:31 +0200

    add typed routes option to configuration with workaround for nextjs issue #67822

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
+
+const nextConfig = (phase) => {
+
+    /** @type {import('next').NextConfig} */
+    const nextConfigOptions = {
+        experimental: {
+            // experimental typescript "statically typed links"
+            // https://nextjs.org/docs/app/api-reference/next-config-js/typedRoutes
+            // currently false in prod until PR #67824 lands in a stable release
+            // https://github.com/vercel/next.js/pull/67824
+            typedRoutes: phase === PHASE_DEVELOPMENT_SERVER ? true : false,
+        },
+    }
+
+    return nextConfigOptions
+
+}
 
 export default nextConfig;
