3f538c18fd5689abc5c7c2b0b3bc70ab0c4e4bce
chrisweb <chris963@gmail.com>
2024-08-20 16:39:18 +0200

    more css examples

--- components/tutorial_examples/UI/Button.tsx
+++ components/tutorial_examples/UI/Button.tsx
@@ -1,12 +1,12 @@
 import styles from './button.module.css'
-
+ 
 const UIButton: React.FC = (): JSX.Element => {
-
+ 
     return (
-        <button className={styles.reset}>
+        <button id={styles.myIdStyle} className={`${styles.reset} ${styles.core}`}>
             I&apos;m a UI button
         </button>
     )
 }
-
+ 
 export default UIButton
