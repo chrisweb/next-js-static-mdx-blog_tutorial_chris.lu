4c48b0e93cc5ed611ce25c92661168db6729f59f
chrisweb <chris963@gmail.com>
2024-08-23 22:07:51 +0200

    fix bug

--- components/toc/Highlight.tsx
+++ components/toc/Highlight.tsx
@@ -40,7 +40,7 @@ const TocHighlight: React.FC<IProps> = (props): JSX.Element => {
 
                     child = cloneElement(
                         child,
-                        recursiveChildren(children, activeIdState) as ReactElement<IChildProps>
+                        { children: recursiveChildren(children, activeIdState) as ReactElement<IChildProps> }
                     )
 
                 }
